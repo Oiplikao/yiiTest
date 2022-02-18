9b052719990380a1d0060f62b1dfcfe13315e632
Oiplikao <oiplikao@gmail.com>
2022-02-18 14:55:21 +0400

    Изменил ID БД

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=city_review',
     'username' => 'root',
     'password' => '123',
     'charset' => 'utf8',
